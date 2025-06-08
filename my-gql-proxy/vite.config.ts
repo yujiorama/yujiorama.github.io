9003adb247a61059465b8f5b433303bbcbb4bce4
Yuji Okazawa <yujiorama+github@gmail.com>
2025-06-08 14:58:55 +0900

    Update GitHub Pages workflow and Vite config a base path

--- my-gql-proxy/vite.config.ts
+++ my-gql-proxy/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/yujiorama.github.io/my-gql-proxy/'
+  base: '/my-gql-proxy/'
 })
